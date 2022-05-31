62f36fdec2224c19b54df39c3c48b7c1d42c2262
Ayush-019 <gargayush2002@gmail.com>
2022-05-31 12:27:49 +0530

    .

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const jwt = require("jsonwebtoken");
 const app = express();
 
 app.use(express.json());
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(cookieParser());
 const {
